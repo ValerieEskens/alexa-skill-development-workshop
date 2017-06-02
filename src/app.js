af1e02ace46c9c51937b0c928f0f61e1b5f0c9b7
Valerie Eskens <valerie.eskens@kbc.be>
2017-06-02 14:01:50 +0200

    hello valerie

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-const WELCOME_MESSAGE = 'Yow CG';
+const TEST = "Why don't you come on over Valerie";
+const WELCOME_MESSAGE = 'Welcome to High Low guessing game. You have played 0 times. Would you like to play?';
+const START_GAME = 'Great! Try saying'
 
 
 module.exports.handlers = {
     LaunchRequest() {
-        this.emit(':tell', WELCOME_MESSAGE);
+        this.emit(':tell', TEST);
     }
 };
